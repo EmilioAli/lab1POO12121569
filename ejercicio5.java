1ed60349c9d274768f31c4f210db65b8716c7c98
EmilioAli <143912439+EmilioAli@users.noreply.github.com>
2023-09-03 00:18:25 -0500

    Add files via upload

---
+++ ejercicio5.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduzca un numero para hallar el día de la semana: ");
+        int n = scanner.nextInt();
+
+        switch (n) {
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miércoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            case 6:
+                System.out.println("Sábado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("Número inválido. Ingresa un número del 1 al 7.");
+        }
+
+        scanner.close();
+    }
+}
