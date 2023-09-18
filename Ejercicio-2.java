148f87b8bfd67af96c05f9e3fc8f73a1083dc12f
EmilioAli <143912439+EmilioAli@users.noreply.github.com>
2023-09-18 01:12:36 -0500

    Add files via upload

---
+++ Ejercicio-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+
+public class Main 
+{ 
+    public static void main(String[] args) {
+        String nom, dir, tel; 
+
+        nom = "Antonio Perez";
+        dir  = "Calle Alameda 678";
+        tel = "4780539";
+  
+        System.out.println("Nombres: " + nom); 
+        System.out.println("Direccion: " + dir);
+        System.out.println("Telefono: " + tel);
+    } 
+} 
